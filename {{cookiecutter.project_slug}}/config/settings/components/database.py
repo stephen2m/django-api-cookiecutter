9e1b8e17dde8eae4465d0cb5e29c6df414114fde
Stephen Mue <8377886+stephen2m@users.noreply.github.com>
2024-12-03 15:21:46 +0200

    update database config

--- {{cookiecutter.project_slug}}/config/settings/components/database.py
+++ {{cookiecutter.project_slug}}/config/settings/components/database.py
@@ -1,16 +1,26 @@
 import os
 
+{% if cookiecutter.database == "postgres" %}
+DATABASES = {
+    'default': dj_database_url.config(
+        default='postgres://username:@postgres:5432/postgres',
+        conn_max_age=int(os.getenv('DATABASE_CONN_MAX_AGE', 600)),
+        conn_health_checks=True
+    )
+}
+{% elif cookiecutter.database == "mysql" %}
+DATABASES = {
+    'default': dj_database_url.config(
+        default='mysql://username:@postgres:3306/mysql',
+        conn_max_age=int(os.getenv('DATABASE_CONN_MAX_AGE', 600)),
+        conn_health_checks=True
+    )
+}
+{% else %}
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST', 'localhost'),
-        'PORT': os.getenv('DB_PORT', '5432'),
-        'CONN_MAX_AGE': 60,
-        'OPTIONS': {
-            'connect_timeout': 10,
-        },
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'db.sqlite3',
     }
 }
+{% endif %}
